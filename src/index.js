08536d36656364627a784d22c656c2b28679b84f
Antonin PIDET <Antonin.pidet@gmail.com>
2024-03-24 11:52:54 +0100

    Starting the project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./pages/Home/Home";
+import "./styles/main.css";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='*' element={<React.Fragment><p>Oupsi error 404</p><Link to={"/"}>Test home</Link></React.Fragment>} />
+      </Routes>
+    </Router>
+  </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import "../../styles/main.css";
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Project started
+        </p>
+        <Link to={"/nbhjjhhgchjnbjh,nhgc,hj,hgc,"}>Test erreur 404</Link>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
