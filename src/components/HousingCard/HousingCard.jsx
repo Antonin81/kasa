6c9e22191fadc2b6c38ade903022b580a8dea05d
Antonin PIDET <Antonin.pidet@gmail.com>
2024-03-29 17:27:05 +0100

    Petits changements

--- src/components/HousingCard/HousingCard.jsx
+++ src/components/HousingCard/HousingCard.jsx
@@ -2,11 +2,8 @@ import { Link } from "react-router-dom";
 
 function HousingCard({ img, link }) {
   return (
-    <Link
-      className="housing-card"
-      style={{ "--test": `url(${img})` }}
-      to={link}
-    >
+    <Link className="housing-card" to={link}>
+      <img src={img} alt="" />
       <p>Titre de la location</p>
     </Link>
   );
