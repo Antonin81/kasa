31c8e4e1a679eca54aa6f1be35c5b6319ecd5c78
Antonin PIDET <Antonin.pidet@gmail.com>
2024-04-12 11:43:20 +0200

    avec les nouveaux fichiers c'est mieux

---
+++ src/components/SlideShow/SlideShow.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+function SlideShow({ images }) {
+  const [counter, setCounter] = useState(0);
+  const nbMax = images.length;
+
+  function changeSlide(e) {
+    if (e.target.classList.contains("slideshow-button-left")) {
+      setCounter(counter === 0 ? nbMax - 1 : counter - 1);
+    } else {
+      setCounter(counter === nbMax - 1 ? 0 : counter + 1);
+    }
+  }
+
+  return (
+    <div className="slideshow">
+      <button
+        className="slideshow-button slideshow-button-left"
+        onClick={changeSlide}
+      >
+        <img src="/slideshow-arrow.png" alt="" />
+      </button>
+      <p className="slideshow-counter">
+        {counter + 1}/{nbMax}
+      </p>
+      <img src={images[counter]} alt="" />
+      <button
+        className="slideshow-button slideshow-button-right"
+        onClick={changeSlide}
+      >
+        <img src="/slideshow-arrow.png" alt="" />
+      </button>
+    </div>
+  );
+}
+
+export default SlideShow;
