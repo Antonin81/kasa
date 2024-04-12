d3abe31c15f96f5fca8578882076b8ee58a20050
Antonin PIDET <Antonin.pidet@gmail.com>
2024-04-12 14:46:20 +0200

    better texts

--- src/pages/More/More.jsx
+++ src/pages/More/More.jsx
@@ -9,29 +9,25 @@ function More() {
         <Collapse
           title="Fiabilité"
           texts={[
-            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum quidem necessitatibus officiis velit reprehenderit nam ipsam nihil laborum qui dolorem sint beatae distinctio facilis corrupti alias debitis, libero quae? Quae? Vero dolorum voluptatibus, enim corporis illo odit est deserunt eaque et minus libero consequuntur. Maxime a ipsum porro rem blanditiis quis nobis eaque, vero nulla, modi temporibus magni optio provident? In quas voluptate rerum fugiat enim ab nesciunt illo id ut earum reiciendis officia autem odio praesentium quisquam cum unde, nostrum dolorem numquam repellat molestias, deserunt officiis. Corrupti, iure obcaecati! Neque hic provident error numquam in amet perferendis doloremque quae corrupti cupiditate? Porro aliquam quis et sed cumque excepturi similique, voluptatibus, tenetur voluptate ducimus dolore quas voluptas quidem! Alias, quam!",
+            "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.",
           ]}
         />
         <Collapse
           title="Respect"
           texts={[
-            "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
-            "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+            "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
           ]}
         />
         <Collapse
           title="Service"
           texts={[
-            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
-            "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
-            "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+            "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
           ]}
         />
         <Collapse
           title="Sécurité"
           texts={[
-            "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
-            "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+            "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
           ]}
         />
       </div>
