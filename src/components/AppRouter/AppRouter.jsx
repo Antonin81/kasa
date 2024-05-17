7b1747910e7610cfd1ced75f4230e61adf128165
Antonin PIDET <Antonin.pidet@gmail.com>
2024-05-17 07:59:49 +0200

    Router déplacé dans un composant

---
+++ src/components/AppRouter/AppRouter.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import Home from "../../pages/Home/Home";
+import "../../assets/styles/main.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import HousingDetails from "../../pages/HousingDetails/HousingDetails";
+import More from "../../pages/More/More";
+import Layout from "../Layout/Layout";
+import Error from "../../pages/Error/Error";
+
+function AppRouter() {
+  return (
+    <React.Fragment>
+      <React.StrictMode>
+        <Router>
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <Layout>
+                  <Home />
+                </Layout>
+              }
+            />
+            <Route
+              exact
+              path="/a-propos"
+              element={
+                <Layout>
+                  <More />
+                </Layout>
+              }
+            />
+            <Route
+              exact
+              path="/fiche-logement/:selectedHousing"
+              element={
+                <Layout>
+                  <HousingDetails />
+                </Layout>
+              }
+            />
+            <Route
+              path="/error"
+              element={
+                <Layout>
+                  <Error />
+                </Layout>
+              }
+            />
+            <Route path="*" element={<Navigate to="/error" />} />
+          </Routes>
+        </Router>
+      </React.StrictMode>
+    </React.Fragment>
+  );
+}
+
+export default AppRouter;
--- src/index.jsx
+++ src/index.jsx
@@ -1,63 +1,6 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import Home from "./pages/Home/Home";
-import "./assets/styles/main.css";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from "react-router-dom";
-import HousingDetails from "./pages/HousingDetails/HousingDetails";
-import More from "./pages/More/More";
-import Layout from "./components/Layout/Layout";
-import Error from "./pages/Error/Error";
+import AppRouter from "./components/AppRouter/AppRouter";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-  <React.Fragment>
-    <React.StrictMode>
-      <Router>
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={
-              <Layout>
-                <Home />
-              </Layout>
-            }
-          />
-          <Route
-            exact
-            path="/a-propos"
-            element={
-              <Layout>
-                <More />
-              </Layout>
-            }
-          />
-          <Route
-            exact
-            path="/fiche-logement/:selectedHousing"
-            element={
-              <Layout>
-                <HousingDetails />
-              </Layout>
-            }
-          />
-          <Route
-            path="/error"
-            element={
-              <Layout>
-                <Error />
-              </Layout>
-            }
-          />
-          <Route path="*" element={<Navigate to="/error" />} />
-        </Routes>
-      </Router>
-    </React.StrictMode>
-  </React.Fragment>,
-);
+root.render(<AppRouter />);
